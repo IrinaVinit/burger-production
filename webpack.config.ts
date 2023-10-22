fa734d508f51bc2f17f9f23882a137508befa494
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-22 16:56:27 +0300

    refactor: decomposition of webpack config - plugins, resolvers, loaders

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { buildPlugins } from "./config/build/buildPlugins";
+import { buildLoaders } from "./config/build/buildLoaders";
+import { buildResolvers } from "./config/build/buildResolvers";
 
 const config: webpack.Configuration = {
   mode: "development",
@@ -10,26 +12,13 @@ const config: webpack.Configuration = {
     filename: "[name].[contenthash].js",
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
+  plugins: buildPlugins(),
   module: {
     // здесь обрабатываются файлы, которые выходяь за рамки js. Для этого нужны разные лоадеры
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
+    rules: buildLoaders(),
   },
   // в resolve указываем расширения, для которых при импорте не будем это расширение указывать
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+  resolve: buildResolvers(),
 };
 
 export default config;
