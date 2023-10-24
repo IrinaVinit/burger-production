491483fd86bd2b238b0b6758e6950cf3338e9383
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-24 17:34:58 +0300

    feat: add suspense with fallback

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import "./index.scss";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { FeedPageAsync } from "./pages/FeedPage/FeedPage.async";
 import { LoginPageAsync } from "./pages/LoginPage/LoginPage.acync";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="app">
       <AppHeader />
-      <Routes>
-        <Route path="/" element={<MainPageAsync />} />
-        <Route path='/feed' element={<FeedPageAsync/>}/>
-        <Route path ="/login" element={<LoginPageAsync/>}/>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/feed" element={<FeedPageAsync />} />
+          <Route path="/login" element={<LoginPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
