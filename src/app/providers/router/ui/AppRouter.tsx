9a667a9e3fbd5ca1ca937a732ab7abf3d70de7e2
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-27 10:57:26 +0300

    refactor: add routeConfig to AppRouter

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,17 +1,16 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-// import { routeConfig } from "shared/config/routeConfig/routeConfig";
-import { FeedPage } from "pages/FeedPage";
-import { LoginPage } from "pages/LoginPage";
-import { MainPage } from "pages/MainPage";
+import { routeConfig } from "sheared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/feed" element={<FeedPage/>} />
-      <Route path="/login" element={<LoginPage />} />
+     {Object.values(routeConfig).map(({element, path}) => (
+      <Route key = {path} 
+      element = {element}
+      path = {path}/>
+     ))}
     </Routes>
   </Suspense>
   );
