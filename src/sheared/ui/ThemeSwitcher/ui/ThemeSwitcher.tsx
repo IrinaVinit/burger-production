bcbb41e3fce14e8b29640519b32ca1d61affceaa
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-11-01 22:52:43 +0300

    feat: add stylelint

--- src/sheared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/sheared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'sheared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import ThemeIcon from 'sheared/assets/icons/theme-icon.svg';
 import { Button, ThemeButton } from 'sheared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
   className?: string
@@ -13,7 +12,7 @@ export const ThemeSwitcher = ({ className = '' }: ThemeSwitcherProps) => {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.themeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toogleTheme}
         >
             {theme === 'dark' ? <ThemeIcon fill="#FFC700" /> : theme === 'light' ? <ThemeIcon fill="#0115C6" /> : <ThemeIcon fill="#0a0a0a" />}
